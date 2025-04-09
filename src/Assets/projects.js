035cb62362582bb9e6d6edb4fe1eb9ab9246b91a
MukulTiwari02 <mukultiwari2020@gmail.com>
2025-04-10 05:19:10 +0530

    Changes in projects section

---
+++ src/Assets/projects.js
@@ -0,0 +1,44 @@
+const projectArray = [
+  {
+    name: "AI Developer Chat",
+    link: "https://ai-dev-chat.vercel.app/",
+    codeLink: "https://github.com/MukulTiwari02/AI-DevChatApp",
+  },
+  {
+    name: "URL Shortener",
+    link: "https://url-shortener-app-two.vercel.app/",
+    codeLink: "https://github.com/MukulTiwari02/URL_Shortener_App",
+  },
+  {
+    name: "Progressly",
+    link: "https://progressly-demo.vercel.app/",
+    codeLink: "https://github.com/MukulTiwari02/Progressly-landing-page",
+  },
+  {
+    name: "Mini Games Hub",
+    link: "https://mini-games-js.vercel.app/",
+    codeLink: "https://github.com/MukulTiwari02/mini-games-js",
+  },
+  {
+    name: "Expense Tracker",
+    link: "https://expensetracker-mt.vercel.app/",
+    codeLink: "https://github.com/MukulTiwari02/MERN-expenseTracker",
+  },
+  {
+    name: "Home Security System - Arduino",
+    link: "",
+    codeLink: "https://github.com/MukulTiwari02/HomeSecuritySystem-Arduino",
+  },
+  {
+    name: "Social Media App",
+    link: "",
+    codeLink: "https://github.com/MukulTiwari02/social-media-app",
+  },
+  {
+    name: "AI - URL to Video",
+    link: "",
+    codeLink: "https://github.com/MukulTiwari02/AI-url2Video",
+  },
+];
+
+export default projectArray;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,30 +1,20 @@
-import { link, section } from "motion/react-client";
 import React from "react";
+import projectArray from "../Assets/projects";
 
 const Projects = () => {
-  const projectArray = [
-    { name: "AI Developer Chat", link: "https://ai-dev-chat.vercel.app/" },
-    {
-      name: "URL Shortener",
-      link: "https://url-shortener-app-two.vercel.app/",
-    },
-    { name: "Progressly", link: "https://progressly-demo.vercel.app/" },
-    { name: "Mini Games Hub", link: "https://mini-games-js.vercel.app/" },
-    { name: "Expense Tracker", link: "https://expensetracker-mt.vercel.app/" },
-  ];
   const [currentSelectedProject, setCurrentSelectedProject] = React.useState(
     projectArray[0]
   );
 
   return (
-    <section id="Projects">
+    <section className="scroll-mt-[10rem] sm:scroll-mt-[13rem]" id="Projects">
       <h1
         className="text-xl sm:text-[2.5rem] w-full text-center mb-10"
         style={{ color: "var(--secondary-color" }}
       >
         Crafted With Code
       </h1>
-      <div className="projects w-full h-[500px] flex">
+      <div className="projects w-full mb-16 h-[500px] flex">
         <div className="project-list text-white h-full max-w-[50%] sm:flex-grow px-5 sm:px-20">
           <h1
             className="text-xl sm:text-4xl sm:-mt-6"
@@ -32,7 +22,7 @@ const Projects = () => {
           >
             Projects
           </h1>
-          <div className="flex flex-col gap-2 sm:gap-5 mt-5 h-full overflow-x-hidden">
+          <div className="flex flex-col gap-0.5 mt-5 h-full overflow-x-hidden">
             {projectArray.map((project) => (
               <button
                 onClick={() => {
@@ -40,13 +30,20 @@ const Projects = () => {
                     return setCurrentSelectedProject(project);
                 }}
                 className={
-                  "transition-none text-sm sm:text-xl text-left p-3 sm:p-6 hover:bg-green-500 " +
+                  "transition-none flex flex-col gap-2 sm:flex-row text-sm sm:text-xl text-left p-3 sm:p-6 hover:bg-green-500 " +
                   (currentSelectedProject.name === project.name
                     ? "bg-green-500"
                     : " bg-[rgb(231,104,65)]")
                 }
               >
-                {project.name}
+                <div>{project.name}</div>
+                <a
+                  target="_blank"
+                  className="ml-auto text-black"
+                  href={project.codeLink}
+                >
+                  Code
+                </a>
               </button>
             ))}
           </div>
@@ -54,13 +51,15 @@ const Projects = () => {
 
         <div className="project-container text-white h-full w-[50%] relative">
           <div className="h-full w-[220%] sm:w-[180%]  -translate-x-[30%] sm:-translate-x-[25%] scale-[0.40] sm:scale-[0.45] absolute top-0 left-0 flex justify-center items-center">
-            <a
-              href={currentSelectedProject.link}
-              target="_blank"
-              className="absolute block hover:scale-105 transition-all -bottom-60 z-10 left-[6%] sm:left-[2%]  text-lg sm:text-3xl text-white w-fit py-3 px-4 sm:py-5 sm:px-6 rounded-xl bg-[rgb(231,104,65)]"
-            >
-              Full Screen
-            </a>
+            {currentSelectedProject.link !== "" && (
+              <a
+                href={currentSelectedProject.link}
+                target="_blank"
+                className="absolute block hover:scale-105 transition-all -bottom-60 z-10 left-[6%] sm:left-[2%]  text-lg sm:text-3xl text-white w-fit py-3 px-4 sm:py-5 sm:px-6 rounded-xl bg-[rgb(231,104,65)]"
+              >
+                Full Screen
+              </a>
+            )}
 
             <div className="relative w-[1920px] h-[1080px] rounded-3xl  bg-[#1f1f1f] shadow-xl shadow-gray-800 border-[12px] border-[#333] flex flex-col overflow-hidden">
               <div className="h-12 bg-[#2c2c2c] flex items-center px-4 space-x-2">
@@ -70,12 +69,21 @@ const Projects = () => {
                 <span className="text-white ml-4">Live Preview</span>
               </div>
 
-              <iframe
-                src={currentSelectedProject.link}
-                title="AI Developer Chat Project"
-                className="flex-1 w-full h-full border-none"
-                allowFullScreen
-              ></iframe>
+              {currentSelectedProject.link !== "" && (
+                <iframe
+                  src={currentSelectedProject.link}
+                  title="AI Developer Chat Project"
+                  className="flex-1 w-full h-full border-none"
+                  allowFullScreen
+                ></iframe>
+              )}
+              {currentSelectedProject.link === "" && (
+                <img
+                  src="https://media.istockphoto.com/id/1404059706/vector/website-page-not-found-error-404-oops-worried-robot-character-peeking-out-of-outer-space.jpg?s=612x612&w=0&k=20&c=DvPAUof9UsNuNqCJy2Z7ZLLk75qDA3bbLXOOW_50wAk="
+                  alt="Deployment Not Found"
+                  className="flex-1 sm:w-full sm:h-full border-none sm:object-cover object-scale-down"
+                />
+              )}
             </div>
           </div>
         </div>
